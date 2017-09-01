8f3773fc470f6a05e7c4cf266d7a2fcba0545414
Andrew Lonsway <alonswa@purdue.edu>
2017-09-01 00:37:13 -0500

    Comments to allow out-of-login execution
    Man, Unix is a beaut, innit

--- STARTUP_SCRIPT.sh
+++ STARTUP_SCRIPT.sh
@@ -1,7 +1,8 @@
 # This is the script that mounts a USB and uses it to automatically start any program specified by the startup script on the drive
 # Useful for any embedded system where the executable will be compiled on another device, and no keyboard/monitor IO is needed
-# In order to allow this script to start on boot, add the line "sh ~/AutoTurret/STARTUP_SCRIPT.sh &" to your /etc/rc.local 
-# Also, this script runs again if you log in, so TODO: do not run more than once
+# In order to allow this script to start on boot, add the line "sh ~/AutoTurret/STARTUP_SCRIPT.sh &" to your /etc/rc.local
+# After that, to allow execution permissons without logging in, ensure this script has execute access (sudo chmod -x ~/AutoTurret/STARTUP_SCRIPT.sh)
+# Also, this script runs again if you log in, so TODO: do not run more than once for a boot
 USB_PATH="/usb/media/"
 
 # Mount the USB that we expect to find (bottom left on Raspberry Pi B
