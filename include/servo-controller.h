a5567502298bbd74ce3c4ef81e1334d72a8ff3bf
alonswa <alonswa@purdue.edu>
2017-10-17 10:36:06 -0400

    Removed unused line

--- include/servo-controller.h
+++ include/servo-controller.h
@@ -4,7 +4,6 @@ typedef struct servo_controller {
 	int yPos;
 } servo_controller;
 
-int xPos, yPos;
 void servo_controller_init(struct servo_controller * sc);
 void servo_controller_turn(struct servo_controller * sc, int diffX, int diffY);
 
