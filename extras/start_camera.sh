ec96f74edb50d2c6c46c1a99d41f146e6d399af1
Lonswaya <alonswa@purdue.edu>
2017-11-20 11:35:05 -0500

    Faster start camera script

--- extras/start_camera.sh
+++ extras/start_camera.sh
@@ -1,1 +1,2 @@
-pivid -o - -t 0 -n -vf | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
+sudo modprobe bcm2835-v4l2	
+cvlc v4l2:///dev/video0:width=640:height=480:fps=15 --v4l2-vflip 1 --v4l2-hflip 1 --sout="#transcode{vcodec=mp4v}:std{access=http,mux=ts,dst=:8090/stream}"
